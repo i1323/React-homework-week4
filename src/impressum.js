85059e1430af0d0ca54aa33732022189e3b18e0d
i1323 <64890877+i1323@users.noreply.github.com>
2020-08-10 13:55:17 +0200

    update Github Link & add Stylesheets

--- src/impressum.js
+++ src/impressum.js
@@ -4,7 +4,7 @@ export default function impressum() {
   return (
     <div className="impressum">
       <p class="impressum">
-        <a href="https://github.com/i1323/finalProject1323" target="_blank">
+        <a href="https://github.com/i1323/React-homework-week4" target="_blank">
           Open-source Code
         </a>
         by
